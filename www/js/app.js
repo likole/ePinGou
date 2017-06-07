60e8a29f07d52e5f116cf18d73ba23e6b5532311
likole <likolef@gmail.com>
2017-06-07 18:14:23 +0800

    integrated pages and completed account module and shopList module

--- www/js/app.js
+++ www/js/app.js
@@ -7,7 +7,7 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers'])
 
-  .run(function ($ionicPlatform) {
+  .run(function ($ionicPlatform,$rootScope) {
     $ionicPlatform.ready(function () {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
@@ -21,6 +21,62 @@ angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers'])
         StatusBar.styleDefault();
       }
     });
+
+
+    /**
+     * 测试数据
+     */
+
+    //邮件
+    //status 0 未读 1 已读
+    $rootScope.myEmails=[
+      {'from':'系统通知','content':'欢迎注册E-品购，在这里你可以方便得购买商品，二手商品，同时还能自己开店哟~','status':1},
+      {'from':'系统通知','content':'您的店铺信息已提交申请，请耐心等待~','status':1},
+      {'from':'系统通知','content':'您的店铺已通过验证，可以运营~','status':0},
+    ];
+
+    //留言
+    //0 未读 1 已读
+    $rootScope.myMessage=[
+      {'from':'沈鹏杰','content':'小姐服务 +Q1455053196','status':0},
+      {'from':'沈鹏杰','content':'哥哥，哥哥，来陪我玩嘛~','status':0},
+    ];
+
+    //商品列表
+    $rootScope.goodsList=[
+      {'gid':0,'name':'辣条零食大礼包1219g馋魔芋亲嘴烧儿时','description':'五款经典卫龙辣味 一套，填充嘴巴的空虚 儿时味道，全新包装，带给你童年的情怀，镇店之宝，不容错过','photo':["https://img.alicdn.com/bao/uploaded/i1/1000906049195202035/TB2SLV9q4XkpuFjy0FiXXbUfFXa_!!0-rate.jpg_400x400.jpg",],'remain':119,'price':26.90,'sold':64,'shop':'印象','like':50},
+      {'gid':1,'name':'3只松鼠手撕寿司面包228g×2','description':'早餐零食糕点手撕面包，居家旅行必备糕点','photo':["https://img.alicdn.com/bao/uploaded/i2/TB1geymRFXXXXXeXpXXXXXXXXXX_!!0-item_pic.jpg_640x640q50.jpg","https://img.alicdn.com/bao/uploaded/i1/880734502/TB2UBeXcC4mpuFjSZFOXXaUqpXa_!!880734502.jpg_1080x1800Q50s50.jpg_.webp","https://img.alicdn.com/bao/uploaded/i3/880734502/TB2AqknX0XnpuFjSZFoXXXLcpXa_!!880734502.jpg_1080x1800Q50s50.jpg_.webp","https://img.alicdn.com/bao/uploaded/i2/880734502/TB2FA8HoSFmpuFjSZFrXXayOXXa_!!880734502.jpg_1080x1800Q50s50.jpg_.webp",],'remain':119,'price':14.90,'sold':121,'shop':'印象','like':138},
+      {'gid':2,'name':'工业专业订制包装盒','description':'厂家定做各种包装盒礼品盒，欢迎各位朋友进店选购，前100名有优惠哦~','photo':["http://gw.alicdn.com/imgextra/i2/2741318793/TB2docMqXXXXXXdXXXXXXXXXXXX_!!2741318793.jpg_970x970q50s150.jpg_.webp","http://gw.alicdn.com/imgextra/i2/2741318793/TB2R2qZppXXXXcUXXXXXXXXXXXX_!!2741318793.jpg_970x970q50s150.jpg_.webp","http://gw.alicdn.com/imgextra/i1/2741318793/TB2zuATuXXXXXbGXpXXXXXXXXXX_!!2741318793.jpg_970x970q50s150.jpg_.webp","http://gw.alicdn.com/imgextra/i2/2741318793/TB2Thy9qt4opuFjSZFLXXX8mXXa_!!2741318793.jpg_970x970q50s150.jpg_.webp",],'remain':119,'price':1.00,'sold':288,'shop':'印象','like':32},
+      {'gid':3,'name':'晨光黑科技，考试必备大容量中性笔','description':'还在为考试的时候没有水而苦恼吗，那么你的福音来了，快来抢购吧','photo':["https://img.alicdn.com/bao/uploaded/i1/TB1HAqPQFXXXXcPXFXXXXXXXXXX_!!0-item_pic.jpg_640x640q50.jpg","https://img.alicdn.com/bao/uploaded/i4/1584214999/TB2G2Iyml0kpuFjy1zdXXXuUVXa_!!1584214999.jpg_1080x1800Q50s50.jpg_.webp","https://img.alicdn.com/bao/uploaded/i1/1584214999/TB2EbzfoNlmpuFjSZPfXXc9iXXa_!!1584214999.jpg_1080x1800Q50s50.jpg_.webp","https://img.alicdn.com/bao/uploaded/i1/1584214999/TB2PLJsl3RkpuFjy1zeXXc.6FXa_!!1584214999.jpg_1080x1800Q50s50.jpg_.webp",],'remain':119,'price':17.80,'sold':1702,'shop':'印象','like':524},
+      {'gid':4,'name':'买一送12 雅丽洁植物水保温补水护肤化妆水','description':'你爱美吗？那么我就让你好看！暑期大酬宾，你值得拥有！','photo':["https://img.alicdn.com/bao/uploaded/i1/2676212008/TB23WUck4dkpuFjy0FbXXaNnpXa_!!2676212008.jpg_1080x1800Q50s50.jpg_.webp","https://img.alicdn.com/bao/uploaded/i1/2676212008/TB23WUck4dkpuFjy0FbXXaNnpXa_!!2676212008.jpg_1080x1800Q50s50.jpg_.webp","https://img.alicdn.com/bao/uploaded/i3/2676212008/TB2GzfFqVXXXXcFXXXXXXXXXXXX_!!2676212008.jpg_1080x1800Q50s50.jpg_.webp","https://img.alicdn.com/bao/uploaded/i2/2676212008/TB2X31WoFXXXXclXXXXXXXXXXXX_!!2676212008.jpg_1080x1800Q50s50.jpg_.webp",],'remain':119,'price':17.50,'sold':9462,'shop':'印象','like':1204},
+      {'gid':5,'name':'上海特产 立丰原味猪肉脯（纯猪肉）','description':'正品猪肉脯','photo':["http://gw.alicdn.com/imgextra/i4/118104546/TB2Y2O.aUhnpuFjSZFpXXcpuXXa_!!118104546.jpg_970x970q50s150.jpg_.webp","http://gw.alicdn.com/imgextra/i4/118104546/TB2zQS_aNBmpuFjSZFsXXcXpFXa_!!118104546.jpg_970x970q50s150.jpg_.webp","http://gw.alicdn.com/imgextra/i2/118104546/TB2OPfcaNlmpuFjSZPfXXc9iXXa_!!118104546.jpg_970x970q50s150.jpg_.webp","http://gw.alicdn.com/imgextra/i1/118104546/TB2HaraaHlmpuFjSZFlXXbdQXXa_!!118104546.jpg_970x970q50s150.jpg_.webp",],'remain':119,'price':60.00,'sold':534,'shop':'印象','like':8465},
+      {'gid':6,'name':'三只松鼠_混合坚果组合','description':'热卖中....','photo':["https://img.alicdn.com/bao/uploaded/i4/880734502/TB2Yj4CgYJkpuFjy1zcXXa5FFXa_!!880734502.jpg_1080x1800Q50s50.jpg_.webp","https://img.alicdn.com/bao/uploaded/i4/880734502/TB2wqaCoFXXXXa9XpXXXXXXXXXX_!!880734502.jpg_1080x1800Q50s50.jpg_.webp","https://img.alicdn.com/bao/uploaded/i1/880734502/TB234WSoFXXXXaWXXXXXXXXXXXX_!!880734502.jpg_1080x1800Q50s50.jpg_.webp","https://img.alicdn.com/bao/uploaded/i4/880734502/TB2Yj4CgYJkpuFjy1zcXXa5FFXa_!!880734502.jpg_1080x1800Q50s50.jpg_.webp",],'remain':119,'price':24.00,'sold':98,'shop':'印象','like':22},
+      {'gid':7,'name':'可爱棉麻抱枕','description':'图案可自选','photo':["http://gw.alicdn.com/imgextra/i3/1863643188/TB29pLFcCvHfKJjSZFPXXbttpXa_!!1863643188.jpg_970x970q50s150.jpg_.webp","http://gw.alicdn.com/imgextra/i4/1852982263/TB2d.iCpgxlpuFjSszbXXcSVpXa_!!1852982263.jpg_970x970q50s150.jpg_.webp","http://gw.alicdn.com/imgextra/i3/1852982263/TB2AAg.v5lnpuFjSZFgXXbi7FXa_!!1852982263.jpg_970x970q50s150.jpg_.webp","http://gw.alicdn.com/imgextra/i1/1852982263/TB2UZdQsB0kpuFjSsppXXcGTXXa_!!1852982263.jpg_970x970q50s150.jpg_.webp",],'remain':119,'price':57.00,'sold':51,'shop':'印象','like':65},
+      {'gid':8,'name':'有线 机械键盘 达尔优 Cherry樱桃 海盗船','description':'超神不再是梦！','photo':["https://img.alicdn.com/bao/uploaded/i2/765917784/TB2axV_m88kpuFjSspeXXc7IpXa_!!765917784.jpg_1080x1800Q50s50.jpg_.webp","https://img.alicdn.com/bao/uploaded/i3/TB1D.EYQFXXXXbRXVXXXXXXXXXX_!!0-item_pic.jpg_640x640q50.jpg","https://img.alicdn.com/bao/uploaded/i1/765917784/TB2OCNdc2OM.eBjSZFqXXculVXa_!!765917784.jpg_1080x1800Q50s50.jpg_.webp","https://img.alicdn.com/bao/uploaded/i1/765917784/TB2zjJVkC8mpuFjSZFMXXaxpVXa_!!765917784.jpg_1080x1800Q50s50.jpg_.webp",],'remain':119,'price':199.00,'sold':94,'shop':'印象','like':665},
+      {'gid':9,'name':'Java从入门到精通 零基础学编程','description':'助你完成程序猿的梦想','photo':["https://img.alicdn.com/bao/uploaded/i2/TB1Yyk5QVXXXXX1apXXXXXXXXXX_!!0-item_pic.jpg_640x640q50.jpg","https://img.alicdn.com/bao/uploaded/i3/684389695/TB2fR3Lor0kpuFjy0FjXXcBbVXa_!!684389695.jpg_1080x1800Q50s50.jpg_.webp","https://img.alicdn.com/bao/uploaded/i3/684389695/TB29l7GoCBjpuFjSsplXXa5MVXa_!!684389695.jpg_1080x1800Q50s50.jpg_.webp","https://img.alicdn.com/bao/uploaded/i2/684389695/TB2TLBOpa8lpuFjy0FpXXaGrpXa_!!684389695.jpg_1080x1800Q50s50.jpg_.webp",],'remain':119,'price':47.80,'sold':84,'shop':'印象','like':35},
+    ];
+
+    //交易记录
+    //gid 商品编号
+    //status 0 订单生效 1 交易完成 2 订单取消
+    $rootScope.myTransaction=[
+      {'time':'2017-5-20 16:33:21','num':2,'price':53.80,'status':1,'name':'辣条零食大礼包1219g馋魔芋亲嘴烧儿时','photo':"https://img.alicdn.com/bao/uploaded/i1/1000906049195202035/TB2SLV9q4XkpuFjy0FiXXbUfFXa_!!0-rate.jpg_400x400.jpg"},
+      {'time':'2017-5-27 08:35:51','num':4,'price':1.00,'status':1,'name':'脆司令干吃面','photo':"https://img.alicdn.com/bao/uploaded/i2/TB1pcwTOVXXXXbmapXXXXXXXXXX_!!0-item_pic.jpg"},
+      {'time':'2017-5-31 13:21:23','num':3,'price':38.00,'status':1,'name':'肉松饼','photo':"https://img.alicdn.com/bao/uploaded/i2/2129965087/TB2yVfVpHlmpuFjSZFlXXbdQXXa_!!2129965087.jpg_1080x1800Q50s50.jpg_.webp"},
+      {'time':'2017-6-01 18:41:59','num':12,'price':8.00,'status':1,'name':'百事可乐','photo':"http://gw.alicdn.com/imgextra/i4/3167665387/TB2ANPkp1J8puFjy1XbXXagqVXa_!!3167665387.png_970x970.jpg_.webp"},
+      {'time':'2017-6-05 21:40:32','num':4,'price':4.00,'status':1,'name':'康师傅红烧桶面','photo':"http://gw.alicdn.com/imgextra/i1/2861084049/TB2ED5sc_cCL1FjSZFPXXXZgpXa_!!2861084049.jpg_970x970q50s150.jpg_.webp"},
+      {'time':'2017-6-08 19:47:25','num':2,'price':6.00,'status':1,'name':'超值泡面搭档火腿肠','photo':"https://img.alicdn.com/bao/uploaded/i2/3193172982/TB2AlOBjH_0UKFjy1XaXXbKfXXa_!!3193172982.jpg_1080x1800Q50s50.jpg_.webp"},
+    ];
+
+    //好友列表
+    //avatar 头像
+    $rootScope.myFriends=[
+      {'fid':0,'name':'南剑','lastMessage':'社会你剑姐','unread':3,'avatar':"http://www.92pig.com/data/upload/allimg/20160128/56a9be71a7d8c.jpg"},
+      {'fid':1,'name':'陈丰','lastMessage':'社会你丰哥','unread':0,'avatar':"https://avatars0.githubusercontent.com/u/24356441?v=3&s=460"},
+      {'fid':2,'name':'周慧鹏','lastMessage':'社会你鸭哥','unread':0,'avatar':"http://pic1.58cdn.com.cn/p1/big/n_v1bl2lwkf2vuuvpmkqkblq.jpg"},
+      {'fid':3,'name':'刘璐','lastMessage':'社会你璐姐','unread':1,'avatar':"http://img1.touxiang.cn/uploads/20120509/09-014623_68.jpg"},
+      {'fid':4,'name':'QQ小冰','lastMessage':'我叫社会。。','unread':2,'avatar':"http://img02.store.sogou.com/app/a/10010016/debcfa5fbc02ae098fce38de1550769a"},
+    ];
   })
 
   .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
@@ -67,13 +123,66 @@ angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers'])
           }
         }
       })
-
+      .state('tab.myemail', {
+        url: '/myemail',
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/tab-myemail.html',
+            controller: 'myemailCtrl'
+          }
+        }
+      })
+      .state('tab.myfriends', {
+        url: '/myfriends',
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/tab-myfriends.html',
+            controller: 'myfriendsCtrl'
+          }
+        }
+      })
+      .state('tab.myleavemessage', {
+        url: '/myleavemessage',
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/tab-myleavemessage.html',
+            controller: 'myleavemessageCtrl'
+          }
+        }
+      })
+      .state('tab.transaction', {
+        url: '/transaction',
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/account-transaction.html',
+            controller: 'transactionCtrl'
+          }
+        }
+      })
+      .state('tab.myshop', {
+        url: '/myshop',
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/tab-myshop.html',
+            controller: 'myshopCtrl'
+          }
+        }
+      })
       .state('tab.shop', {
         url: '/shop',
         views: {
           'tab-shop': {
             templateUrl: 'templates/tab-shop.html',
             controller: 'shopCtrl'
+          }
+        }
+      })
+      .state('tab.goodDetail', {
+        url: '/goodDetail/:id',
+        views: {
+          'tab-shop': {
+            templateUrl: 'templates/shop-detail.html',
+            controller: 'detailCtrl'
           }
         }
       })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,99 +1,157 @@
 angular.module('starter.controllers', [])
-  .controller("loginCtrl",function ($scope,$ionicSideMenuDelegate,$rootScope,$state,$http) {
-    $rootScope.showSideMenu=true;
+  .controller("loginCtrl", function ($scope, $ionicSideMenuDelegate, $rootScope, $state, $http) {
+    $rootScope.showSideMenu = true;
 
 
     //判断是否需要登陆
-    if(localStorage.getItem("login")=="true")
-    {
+    //if(localStorage.getItem("login")=="true")
+    //{
 
-      //无需登陆
-      $state.go('tab.shop');
-    }else{
-
-      //需要登陆
-      $rootScope.showSideMenu=false;
-      $ionicSideMenuDelegate.canDragContent(false);
-    };
-
-    $scope.phone=null;
-    $scope.password=null;
-
-    //登陆
-    $scope.loginUser=function () {
-      $http.get("http://123.206.116.70:8080/ePinGou/user_login?phone")
-        .then(function (response) {
-          $scope.res=response;
-          if(response.data.status==0)
-          {
-            $state.go('tab.shop');
-          }else
-          {
-            $state.go('tab.second');
-          }
-        });
-    };
+    //无需登陆
+    $state.go('tab.shop');
+    //}else{
+    //
+    //  //需要登陆
+    //  $rootScope.showSideMenu=false;
+    //  $ionicSideMenuDelegate.canDragContent(false);
+    //};
+    //
+    //$scope.phone=null;
+    //$scope.password=null;
+    //
+    ////登陆
+    //$scope.loginUser=function () {
+    //  $http.get("http://123.206.116.70:8080/ePinGou/user_login?phone")
+    //    .then(function (response) {
+    //      $scope.res=response;
+    //      if(response.data.status==0)
+    //      {
+    //        $state.go('tab.shop');
+    //      }else
+    //      {
+    //        $state.go('tab.second');
+    //      }
+    //    });
+    //};
 
   })
 
-  //二手市场控制器
-.controller("secondCtrl",function ($scope,$http) {
-  //初始值
-  $scope.goods=[];
-  $scope.next=0;
-  $scope.loadCompleted=0;
+  /**
+   * 二手市场控制器
+   */
+  .controller("secondCtrl", function ($scope, $http) {
 
-  //商品分类
-  $scope.category=[
-    {"name":"手机/配件","icon":"img/a.png"},
-    {"name":"电脑/配件","icon":"img/b.png"},
-    {"name":"书籍资料","icon":"img/e.png"},
-    {"name":"闲置礼品","icon":"img/f.png"},
-    {"name":"衣物箱包","icon":"img/g.png"},
-    {"name":"文体乐器","icon":"img/d.png"},
-    {"name":"自行车","icon":"img/c.png"},
-    {"name":"其他","icon":"img/c.png"}
-  ];
+    //初始值
+    $scope.goods = [];
+    $scope.next = 0;
+    $scope.loadCompleted = 0;
 
-  //刷新
-  $scope.loadMore=function () {
-    $http.get("http://123.206.116.70:8080/ePinGou/secondHand_getList?start="+$scope.next)
-      .then(function (response) {
-        if(response.data.status==0)
-        {
-          $scope.goods = $scope.goods.concat(response.data.list) ;
-          $scope.next=response.data.next;
-
-        }else
-        {
-          $scope.loadCompleted=1;
-        }
-        $scope.$broadcast("scroll.infiniteScrollComplete");
-      });
-
-  }
-  $scope.$on('$stateChangeSuccess', function() {
-    $scope.loadMore();
-  });
-
-})
-.controller("shopCtrl",function ($scope) {
-
-  $scope.goods=[
-    {"name":"卫龙","price":2.5,"last":100},
-    {"name":"百事可乐","price":4,"last":100},
-  ];
-
-  $scope.doRefresh=function () {
-    $scope.goods=[
-      {"name":"卫龙","price":2.5,"last":100},
-      {"name":"百事可乐","price":4,"last":100},
-      {"name":"美年达","price":7,"last":100}
+    //商品分类
+    $scope.category = [
+      {"name": "手机/配件", "icon": "img/a.png"},
+      {"name": "电脑/配件", "icon": "img/b.png"},
+      {"name": "书籍资料", "icon": "img/e.png"},
+      {"name": "闲置礼品", "icon": "img/f.png"},
+      {"name": "衣物箱包", "icon": "img/g.png"},
+      {"name": "文体乐器", "icon": "img/d.png"},
+      {"name": "自行车", "icon": "img/c.png"},
+      {"name": "其他", "icon": "img/c.png"}
     ];
-    $scope.$broadcast('scroll.refreshComplete');
-  }
 
-})
-.controller("accountCtrl",function () {
+    //刷新
+    $scope.loadMore = function () {
+      $http.get("http://123.206.116.70:8080/ePinGou/secondHand_getList?start=" + $scope.next)
+        .then(function (response) {
+          if (response.data.status == 0) {
+            $scope.goods = $scope.goods.concat(response.data.list);
+            $scope.next = response.data.next;
 
-})
+          } else {
+            $scope.loadCompleted = 1;
+          }
+          $scope.$broadcast("scroll.infiniteScrollComplete");
+        });
+
+    }
+    $scope.$on('$stateChangeSuccess', function () {
+      $scope.loadMore();
+    });
+
+  })
+  .controller("shopCtrl", function ($scope) {
+
+    $scope.goods = [
+      {"name": "卫龙", "price": 2.5, "last": 100},
+      {"name": "百事可乐", "price": 4, "last": 100},
+    ];
+
+    $scope.doRefresh = function () {
+      $scope.goods = [
+        {"name": "卫龙", "price": 2.5, "last": 100},
+        {"name": "百事可乐", "price": 4, "last": 100},
+        {"name": "美年达", "price": 7, "last": 100}
+      ];
+      $scope.$broadcast('scroll.refreshComplete');
+    }
+
+  })
+
+  /**
+   * 个人中心控制器
+   */
+  .controller("accountCtrl", function ($scope, $rootScope) {
+
+    //未读消息数目
+    $scope.badgeNum = {'email': 0,'message':0,'friend':0};
+
+    //统计未读消息数目
+    $scope.countNum = function () {
+      $scope.badgeNum = {'email': 0,'message':0,'friend':0};
+      for (var i = 0; i < $rootScope.myEmails.length; i++) {
+        if ($rootScope.myEmails[i].status == 0) $scope.badgeNum.email++;
+      }
+      for (var i = 0; i < $rootScope.myMessage.length; i++) {
+        if ($rootScope.myMessage[i].status == 0) $scope.badgeNum.message++;
+      }
+      for (var i = 0; i < $rootScope.myFriends.length; i++) {
+         $scope.badgeNum.friend+=$rootScope.myFriends[i].unread;
+      }
+    };
+
+    //调用统计数目
+    $scope.countNum();
+
+  })
+  .controller("myemailCtrl", function ($scope, $rootScope) {
+
+  })
+  .controller("myfriendsCtrl", function () {
+
+  })
+  .controller("myleavemessageCtrl", function () {
+
+  })
+  .controller("transactionCtrl", function () {
+
+  })
+  .controller("myshopCtrl", function () {
+
+  })
+
+  /**
+   * 商品详情控制器
+   */
+  .controller("detailCtrl", function ($stateParams,$scope) {
+      $scope.id=$stateParams.id;
+      $scope.goodNum=1;
+      $scope.minus=function () {
+        $scope.goodNum--;
+        $scope.$apply();
+      };
+      $scope.add=function () {
+        $scope.goodNum++;
+        $scope.$apply();
+      }
+  })
+
+
